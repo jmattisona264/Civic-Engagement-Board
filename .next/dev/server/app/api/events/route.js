bbf989eab53524425de846f1cbbf06505a59e5d1
Jared Mattison <jmattisona264@gmail.com>
2026-07-26 22:11:20 -0400

    Remove .next build cache from Git tracking

--- .next/dev/server/app/api/events/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/events/route.js")
-R.c("server/chunks/app_api_events_route_0ajn10l.js")
-R.c("server/chunks/[root-of-the-server]__14poz9c._.js")
-R.c("server/chunks/[root-of-the-server]__1fknj6l._.js")
-R.c("server/chunks/_next-internal_server_app_api_events_route_actions_1ozh0xo.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/events/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/events/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
